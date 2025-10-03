588c5f55e0afa461102f0cac988afac4be60cbda
JKruisheer <kruisheer@outlook.com>
2025-10-03 13:53:04 +0200

    add primeng as a frontend lib

--- frontend/src/app/app.config.ts
+++ frontend/src/app/app.config.ts
@@ -1,12 +1,36 @@
-import { ApplicationConfig, provideBrowserGlobalErrorListeners, provideZoneChangeDetection } from '@angular/core';
-import { provideRouter } from '@angular/router';
+import {ApplicationConfig, provideBrowserGlobalErrorListeners, provideZoneChangeDetection} from '@angular/core';
+import {provideRouter} from '@angular/router';
+import {routes} from './app.routes';
+import {providePrimeNG} from 'primeng/config';
+import Aura from '@primeuix/themes/aura';
+import {definePreset} from '@primeuix/themes';
 
-import { routes } from './app.routes';
+const orangeAura = definePreset(Aura, {
+  semantic: {
+    primary: {
+      50: "#ffe8d1",
+      100: "#ffd1a3",
+      200: "#ffa65e",
+      300: "#ff8c36",
+      400: "#ff7418",
+      500: "#ff7a00",  // ← your provided orange as base
+      600: "#e56e00",
+      700: "#bf5c00",
+      800: "#994a00",
+      900: "#733800"
+    }
+  }
+});
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
-    provideZoneChangeDetection({ eventCoalescing: true }),
-    provideRouter(routes)
+    provideZoneChangeDetection({eventCoalescing: true}),
+    provideRouter(routes),
+    providePrimeNG({
+      theme: {
+        preset: orangeAura
+      }
+    })
   ]
 };
