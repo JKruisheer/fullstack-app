96637a1d6552caaa136c5de04aca6a3c7bc129c2
JKruisheer <kruisheer@outlook.com>
2025-10-03 14:44:14 +0200

    also add a Fe search functionality, for now not necessary to implement query params in the BE

--- frontend/src/app/features/client/components/clients-table/clients-table.component.ts
+++ frontend/src/app/features/client/components/clients-table/clients-table.component.ts
@@ -5,6 +5,9 @@ import {Message} from 'primeng/message';
 import {TableModule} from 'primeng/table';
 import {Checkbox} from 'primeng/checkbox';
 import {FormsModule} from '@angular/forms';
+import {InputIcon} from 'primeng/inputicon';
+import {IconField} from 'primeng/iconfield';
+import {InputText} from 'primeng/inputtext';
 
 @Component({
   selector: 'app-clients-table',
@@ -14,27 +17,44 @@ import {FormsModule} from '@angular/forms';
     Message,
     TableModule,
     Checkbox,
-    FormsModule
+    FormsModule,
+    InputIcon,
+    IconField,
+    InputText
   ],
   templateUrl: './clients-table.component.html'
 })
 export class ClientsTableComponent implements OnInit {
   private readonly clientsService: ClientsControllerService = inject(ClientsControllerService);
   readonly isLoading: WritableSignal<boolean> = signal(true);
-  readonly clients: WritableSignal<ClientResponse[]> = signal([])
   readonly errorMessage: WritableSignal<string> = signal('');
+
+  private readonly originalClientList: WritableSignal<ClientResponse[]> = signal([])
+  readonly filteredClientList: WritableSignal<ClientResponse[]> = signal([])
 
   ngOnInit(): void {
     this.clientsService.getClients().subscribe({
         next: (clients: ClientResponse[]) => {
           this.isLoading.set(false);
-          this.clients.set(clients);
+          this.originalClientList.set(clients);
+          this.filteredClientList.set(clients);
         },
-        error: (err) => {
+        error: () => {
           this.errorMessage.set("Something went wrong with loading the clients.");
           this.isLoading.set(false);
         }
       }
     )
+  }
+
+  filterTable(value: string) {
+    const search = value.toLowerCase();
+    const filtered = this.originalClientList().filter(client =>
+      (client.fullName?.toLowerCase().includes(search) ?? false) ||
+      (client.displayName?.toLowerCase().includes(search) ?? false) ||
+      (client.email?.toLowerCase().includes(search) ?? false) ||
+      (client.location?.toLowerCase().includes(search) ?? false)
+    );
+    this.filteredClientList.set(filtered);
   }
 }
